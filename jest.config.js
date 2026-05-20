5bdfb87e20248f6e50f9bca506aea2d056e61c80
Henry Lan <heneylansh@gmail.com>
2026-05-20 13:29:45 +0800

    test: add unit tests for PlanMode, MCP, i18n

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^obsidian$': '<rootDir>/tests/__mocks__/obsidian.ts',
+  },
+};
---
+++ tests/__mocks__/obsidian.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  Plugin: class Plugin {},
+  Setting: class Setting {
+    setName() { return this; }
+    setDesc() { return this; }
+    addText() { return this; }
+    addToggle() { return this; }
+    addDropdown() { return this; }
+    addSlider() { return this; }
+  },
+  PluginSettingTab: class PluginSettingTab {},
+  ItemView: class ItemView {},
+  WorkspaceLeaf: class WorkspaceLeaf {},
+};
